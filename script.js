2dd45bdf7ab34fa6fb53f83181d5a9bd45b6a3e9
SectionX <h.stoyraitis@gmail.com>
2022-09-27 17:52:51 +0300

    Add touch support

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function activate(e) {
     e.target.classList.add("activated");
 }
 
+function activatetouch(e) {
+    e.target.classList.add("activated");
+}
+
 const board = document.querySelector(".game");
 boardside = +(getComputedStyle(board)['width'].toString()).substr(0,3)-2;
 
@@ -19,7 +23,8 @@ for (let i = 0; i < len; i++) {
     cell = document.createElement('div');
     cell.style = `width:${cellside}px; height:${cellside}px;`;
     cell.classList.add('cell');
-    cell.addEventListener('mouseover', activate)
+    cell.addEventListener('mouseover', activate);
+    cell.addEventListener('touchmove', activatetouch);
     cell.addEventListener('mousedown', () => mousedown = true);
     cell.addEventListener('mouseup', () => mousedown = false);
     cells.push(cell);
